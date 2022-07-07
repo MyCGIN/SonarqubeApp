8fce2e2904a15b03dc7bbc39d4bf95807996f4a3
Chaudhary <rampukar.chaudhary@capgemini.com>
2022-07-07 13:07:08 +0530

    initial stage

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SonarQubeGitHub
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
